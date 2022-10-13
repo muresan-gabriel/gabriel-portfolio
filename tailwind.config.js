d370fca78c338f10376ea20a0d56edfb031f345d
muresan-gabriel <gabiimuresan23@gmail.com>
2022-10-13 21:59:32 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/components/**/*.{js,jsx}", "./public/index.html"],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
