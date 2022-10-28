7a09d7f641524ff2d7a9b1c444ffdc9a4eef30f9
muresan-gabriel <gabiimuresan23@gmail.com>
2022-10-28 23:17:01 +0300

    Might fix ring issues

---
+++ src/data/experience.js
@@ -0,0 +1,17 @@
+const experience = [
+  {
+    position: "Software Engineer",
+    company: "Computacenter",
+    description: "",
+    projectLink: "/portfolio/dev-lib",
+    technologies: ["ServiceNow", "AngularJS", "ServiceNow APIs"],
+    skills: [
+      "ITSM Implementation",
+      "System Administration",
+      "Application Development",
+    ],
+    certifications: ["CSA - Certified System Administrator"],
+  },
+];
+
+export default experience;
