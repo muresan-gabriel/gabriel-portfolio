5c0f71a1a48a81e3b75e7744d62823934e5a7c49
muresan-gabriel <gabiimuresan23@gmail.com>
2022-12-13 00:57:08 +0200

    Update BlogPost.tsx

--- src/pages/blog-post/BlogPost.tsx
+++ src/pages/blog-post/BlogPost.tsx
@@ -10,6 +10,9 @@ export default function BlogPost({ match }) {
   const { name } = useParams();
   const post = blogPosts.find((post) => post.name === name);
   const [progress, setProgress] = useState(0);
+  const [topClasses, setTopClasses] = useState(
+    "fixed hidden top bottom-[1rem] right-[2rem] bg-slate-900 p-3 rounded-full transition duration-1 hover:bg-slate-800"
+  );
 
   function progressBarScroll() {
     let winScroll =
@@ -25,6 +28,15 @@ export default function BlogPost({ match }) {
     window.onscroll = function () {
       progressBarScroll();
     };
+
+    if (window.scrollY > 900)
+      setTopClasses(
+        "fixed block fade-in-1 top bottom-[1rem] right-[2rem] bg-slate-900 p-3 rounded-full transition duration-1 hover:bg-slate-800"
+      );
+    if (window.scrollY < 900)
+      setTopClasses(
+        "fixed hidden top bottom-[1rem] right-[2rem] bg-slate-900 p-3 rounded-full transition duration-1 hover:bg-slate-800"
+      );
   }, [progress]);
 
   return (
@@ -33,6 +45,22 @@ export default function BlogPost({ match }) {
         className="progress fixed top-0 left-0 py-1 m-0 z-[1000] bg-indigo-500"
         style={{ width: progress + "%" }}
       ></div>
+      <a href="#top" className={topClasses}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="#f8fafc"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+          />
+        </svg>
+      </a>
       <div className="information-container flex items-center justiy-center flex-col fade-in-1">
         <Link
           to="/blog"
